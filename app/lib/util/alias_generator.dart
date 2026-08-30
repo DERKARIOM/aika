d9b742a4bc071ad481016edefafe3eeb4277c556
derkariom@gmail.com <Zinder@110997>
2026-08-30 17:24:12 +0100

    fix : 30/08/2026

--- app/lib/util/alias_generator.dart
+++ app/lib/util/alias_generator.dart
@@ -1,15 +1,31 @@
 import 'dart:math';
 
-import 'package:localsend_app/gen/strings.g.dart';
+/// Simple, well-known manga/anime character names used as the default random
+/// device name (replaces the previous "adjective + fruit" combination, e.g.
+/// "Energetic Cherry"). Kept as plain, untranslated proper nouns since these
+/// names are the same in every language.
+const _mangaCharacterNames = [
+  'Naruto', 'Sasuke', 'Sakura', 'Kakashi', 'Itachi', 'Gaara', 'Hinata', 'Shikamaru', 'Boruto', 'Jiraiya',
+  'Luffy', 'Zoro', 'Nami', 'Sanji', 'Chopper', 'Robin', 'Usopp', 'Franky', 'Brook', 'Ace',
+  'Goku', 'Vegeta', 'Gohan', 'Piccolo', 'Trunks', 'Bulma', 'Krillin', 'Frieza', 'Beerus', 'Broly',
+  'Eren', 'Mikasa', 'Armin', 'Levi', 'Historia', 'Reiner', 'Annie', 'Erwin', 'Hange',
+  'Light', 'Ryuk', 'Misa', 'Near', 'Mello',
+  'Tanjiro', 'Nezuko', 'Zenitsu', 'Inosuke', 'Giyu', 'Shinobu', 'Rengoku', 'Muzan',
+  'Deku', 'Bakugo', 'Todoroki', 'Ochaco', 'Iida', 'Kirishima',
+  'Ichigo', 'Rukia', 'Renji', 'Byakuya', 'Orihime', 'Uryu', 'Toshiro',
+  'Edward', 'Alphonse', 'Winry', 'Roy', 'Riza',
+  'Gon', 'Killua', 'Kurapika', 'Leorio', 'Hisoka',
+  'Kirito', 'Asuna', 'Klein', 'Sinon',
+  'Saitama', 'Genos', 'Fubuki',
+  'Yuji', 'Megumi', 'Nobara', 'Gojo', 'Sukuna',
+  'Kaneki', 'Touka',
+  'Natsu', 'Lucy', 'Erza', 'Gray', 'Wendy', 'Happy',
+  'Shinji', 'Rei', 'Asuka', 'Misato',
+  'Ash', 'Pikachu', 'Misty', 'Brock',
+  'Usagi', 'Mamoru', 'Yugi', 'Kaiba', 'Joey',
+];
 
 String generateRandomAlias() {
   final random = Random();
-  final adj = t.aliasGenerator.adjectives;
-  final fruits = t.aliasGenerator.fruits;
-
-  // The combination of both is locale dependent too.
-  return t.aliasGenerator.combination(
-    adjective: adj[random.nextInt(adj.length)],
-    fruit: fruits[random.nextInt(fruits.length)],
-  );
+  return _mangaCharacterNames[random.nextInt(_mangaCharacterNames.length)];
 }
